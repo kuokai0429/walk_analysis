3bf8dfd0bac39104dc909a535d765da48c847c5f
Brian Kuo <35882177+kuokai0429@users.noreply.github.com>
2023-06-28 07:08:27 +0800

    2023.0628.0708

--- common/pose3d_blender/animationScript.py
+++ common/pose3d_blender/animationScript.py
@@ -2,7 +2,7 @@
 # Simple animation scripts.
 
 import numpy as np
-import bpy
+import bpy, bmesh
 import math
 
 import sys
@@ -11,7 +11,7 @@ import os
 import csv
 
 
-def load_subject_strokes_keypoints(subject, subjects_annot_filepath):
+def load_subject_keypoints(subject, subjects_annot_filepath):
 
 #    subject_annot = annot_df.loc[annot_df['subject'] == subject]
 
@@ -29,11 +29,12 @@ def load_subject_strokes_keypoints(subject, subjects_annot_filepath):
     assert os.path.exists(subject_kp_filepath), f"Subject {subject} 3D keypoints file doesn't exist!"
 
     subject_keypoints = np.load(subject_kp_filepath, encoding='latin1', allow_pickle=True)["reconstruction"]
-    subject_strokes_kp = subject_keypoints[int(subject_annot[column_name['start']]):int(subject_annot[column_name['end']])]
+    # subject_kp = subject_keypoints[int(subject_annot[column_name['start']]):int(subject_annot[column_name['end']])]
+    subject_kp = subject_keypoints
 
-    print(subject_strokes_kp.shape)
+    print(subject_kp.shape)
 
-    return subject_strokes_kp
+    return subject_kp
 
 
 def changeCubeLocation(cube, x, y, z, frame):
@@ -65,13 +66,13 @@ if __name__ == "__main__":
     
     ## Change OS working directory
     
-    os.chdir(r"C:\Users\user\Desktop\stroke_analysis")
+    os.chdir(r"C:\Users\user\Desktop\walk_analysis_v2")
     print(os.getcwd())
     
     
     ## Verify Annotation file
 
-    subjects_annot_filepath = f"annotation/stroke_analysis.csv"
+    subjects_annot_filepath = f"annotation/walk_analysis.csv"
     assert os.path.exists(subjects_annot_filepath), "Subjects annotation file doesn't exist!"
 
 
@@ -83,7 +84,7 @@ if __name__ == "__main__":
         "r_hip": 1, "r_knee": 2, "r_foot": 3, "l_hip": 4, "l_knee": 5, "l_foot": 6
         }
     
-    s1_strokes_kp = load_subject_strokes_keypoints("other_f1_left", subjects_annot_filepath)
+    s1_kp = load_subject_keypoints("1-V2-front", subjects_annot_filepath)
     
     
     ## Remove Objects from scene
@@ -111,46 +112,72 @@ if __name__ == "__main__":
                    [5, 6], [0, 7], [7, 8], [8, 9], [9, 10],
                    [8, 11], [11, 12], [12, 13], [8, 14], [14, 15], [15, 16]]
     
-    for i in range(len(s1_strokes_kp[0])):
+    for i in range(len(s1_kp[0])):
         
-        loc = (s1_strokes_kp[0][i][0]*15, s1_strokes_kp[0][i][1]*15, s1_strokes_kp[0][i][1]*15)
+        loc = (s1_kp[0][i][0]*15, s1_kp[0][i][1]*15, s1_kp[0][i][1]*15)
         bpy.ops.mesh.primitive_cube_add(size=0.3, location=loc)
         
-#    for i in range(len(connections)):
-#        
-#        kp1 = np.array([s1_strokes_kp[0][connections[i][0]][0]*15, 
-#            s1_strokes_kp[0][connections[i][0]][1]*15, s1_strokes_kp[0][connections[i][0]][1]*15])
-#        kp2 = np.array([s1_strokes_kp[0][connections[i][1]][0]*15, 
-#            s1_strokes_kp[0][connections[i][1]][1]*15, s1_strokes_kp[0][connections[i][1]][1]*15])
+    
+    ## Create new connector mesh and mesh object and link to scene
+    
+    for i in range(len(connections)):
+        
+        c1 = bpy.data.objects[f'Cube.{str(("%03d"% connections[i][0]))}'] if connections[i][0] != 0 else bpy.data.objects['Cube']
+        c2 = bpy.data.objects[f'Cube.{str(("%03d"% connections[i][1]))}'] if connections[i][1] != 0 else bpy.data.objects['Cube']
+        
+        kp1 = np.array([s1_kp[0][connections[i][0]][0]*15, 
+            s1_kp[0][connections[i][0]][1]*15, s1_kp[0][connections[i][0]][1]*15])
+        kp2 = np.array([s1_kp[0][connections[i][1]][0]*15, 
+            s1_kp[0][connections[i][1]][1]*15, s1_kp[0][connections[i][1]][1]*15])
+    
+        m = bpy.data.meshes.new('connector')
 
-#        x, y, z = kp2[0] - kp1[0], kp2[1] - kp1[1], kp2[2] - kp1[2]
+        bm = bmesh.new()
+        v1 = bm.verts.new( kp1 )
+        v2 = bm.verts.new( kp2 )
+        e  = bm.edges.new([v1,v2])
 
-#        mag = math.sqrt(x**2 + y**2)
-#        theta = math.pi/2 if mag == 0 else math.atan(z/mag)
-#        beta = 0 if x == 0 else math.atan(y/x)
-#            
-#        if (x <= 0 and y >= 0) or (x <= 0 and y <= 0):
-#            beta = math.pi + beta
+        bm.to_mesh(m)
 
-#        bpy.ops.mesh.primitive_cube_add(size=0.3)
-#        cube = bpy.context.scene.objects[f'Cube.{str(("%03d"% (i+17)))}']
+        o = bpy.data.objects.new( 'connector', m )
+        bpy.context.scene.collection.objects.link( o )
 
-#        gap = math.sqrt(x**2 + y**2 + z**2)
+        # Hook connector vertices to respective cylinders
+        for i, cyl in enumerate([ c1, c2 ]):
+            bpy.ops.object.select_all( action = 'DESELECT' )
+            cyl.select_set(True)
+            o.select_set(True)
+            bpy.context.view_layer.objects.active = o # Set connector as active
 
-#        cube.scale.z = gap/2
-#        cube.location = kp1 + (kp2 - kp1)/2
-#        cube.rotation_euler.z = beta
-#        cube.rotation_euler.y = -theta + math.pi/2
+            # Select vertex
+            bpy.ops.object.mode_set(mode='OBJECT')
+            o.data.vertices[i].select = True    
+            bpy.ops.object.mode_set(mode='EDIT')
+
+            bpy.ops.object.hook_add_selob() # Hook to cylinder
+
+            bpy.ops.object.mode_set(mode='OBJECT')
+            o.data.vertices[i].select = False 
+
+        m = o.modifiers.new('Skin', 'SKIN')
+
+        ## New bit starts here
+        m.use_smooth_shade = True
+
+        m = o.modifiers.new('Subsurf', 'SUBSURF' )
+        m.levels = 2
+        m.render_levels = 2
+
+        ## End of new bit
+        bpy.ops.object.select_all( action = 'DESELECT' )    
+        
         
 
     ## Get references to all cubes
     
     bpy.data.objects['Cube'].select_set(True)
-    for i in range(1, len(s1_strokes_kp[0])):
+    for i in range(1, len(s1_kp[0])):
         bpy.data.objects[f'Cube.{str(("%03d"% i))}'].select_set(True)
-        
-#    for i in range(17, 17+len(connections)):
-#        bpy.data.objects[f'Cube.{str(("%03d"% i))}'].select_set(True)
     
     cubes = bpy.context.selected_objects
     print(cubes)
@@ -159,19 +186,13 @@ if __name__ == "__main__":
     ## Insert keyframe at frame one
     
     start_frame = 1
-    for i in range(0, len(s1_strokes_kp[0])):
+    for i in range(0, len(s1_kp[0])):
         cubes[i].keyframe_insert("location", frame=start_frame)
     
 
     ## Change the location of the cubes
     
-    for i in range(len(s1_strokes_kp)):
-        for j in range(len(s1_strokes_kp[0])):
+    for i in range(len(s1_kp)):
+        for j in range(len(s1_kp[0])):
             
-            changeCubeLocation(cubes[j], s1_strokes_kp[i][j][0]*15, s1_strokes_kp[i][j][1]*15, s1_strokes_kp[i][j][2]*15, i)
-            
-#            for k in range(len(connections)):
-#                changeBonesLocation(cubes[j+17], 
-#                    s1_strokes_kp[i][connections[k][0]][0]*15, s1_strokes_kp[i][connections[k][0]][1]*15, s1_strokes_kp[i][connections[k][0]][2]*15, 
-#                    s1_strokes_kp[i][connections[k][1]][0]*15, s1_strokes_kp[i][connections[k][1]][1]*15, s1_strokes_kp[i][connections[k][1]][2]*15,
-#                    i)
+            changeCubeLocation(cubes[j], s1_kp[i][j][0]*15, s1_kp[i][j][1]*15, s1_kp[i][j][2]*15, i)
